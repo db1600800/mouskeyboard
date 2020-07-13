378ca6e45710db2afbd5245752409a089ac47b9d
yyy <yyy@yy.com>
2020-07-13 12:34:50 +0800

    dd

---
+++ find/find/spiders/qqkeyboard.py
@@ -0,0 +1,29 @@
+import win32api
+import time
+#鼠标移动
+import win32con
+
+
+def mouse_move(x,y):
+    win32api.SetCursorPos([x,y])
+
+#鼠标点击，默认左键
+def mouse_click(click_type="left"):
+    if click_type=="left":
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    else:
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP | win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+    time.sleep(0.01)
+
+#鼠标双击击，默认左键
+def mouse_double_click(click_type="left"):
+    if click_type=="left":
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        time.sleep(0.01)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+
+    else:
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP | win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        time.sleep(0.01)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP | win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+    time.sleep(0.01)
