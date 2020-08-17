9b7996e2e24a016d106776bd397f3835bfd51928
yyy <yyy@yy.com>
2020-08-17 21:44:04 +0800

    getmsg

--- find/find/spiders/getMsgFromTouTiao.py
+++ find/find/spiders/getMsgFromTouTiao.py
@@ -18,6 +18,7 @@ def getMsgFromToutiao(startExecuteBtn):
     citys = partOneObjs.keys()
     # citys=['惠州', '北京', '防城港', '呼和浩特', '衡水', '合肥', '杭州', '海南', '哈尔滨', '桂林', '贵阳', '佛山', '福州', '东莞', '大连', '重庆', '长沙','长春', '成都', '常州', '包头', '保定']
     #citys = ['佛山']
+    #page3 dict_keys(['扬州', '淄博', '珠海', '舟山', '威海', '中山', '郑州', '镇江', '漳州', '湛江', '岳阳', '银川', '徐州', '烟台', '厦门', '西宁', '西安', '武汉', '芜湖', '无锡', '温州', '潍坊'])
     searchStr = ""
     requestAddStr = ""
     xiaoquDateAddCount = 30
@@ -208,18 +209,6 @@ def getMsgFromToutiao(startExecuteBtn):
     area1_xiaoqufile.close()
 
 
-def whichCity(city,startExecuteBtn):
-    chose=False
-    if city == "中山":
-        chose = True
-    """
-    if city == "珠海":
-        chose = True
-    
-    if city == "郑州":
-        chose = True
-    """
-    return chose
 
 
 def citynewslist(newsstr,city):
@@ -234,11 +223,12 @@ def citynewslist(newsstr,city):
            contentobj= json.loads(contentStr["content"])
            article_url=contentobj["article_url"]
            if article_url!="":
-            nearby_read_info=contentobj["nearby_read_info"]
+            #nearby_read_info=contentobj["nearby_read_info"]
             title=contentobj["title"]
             abstract=contentobj["abstract"]
             divhtml=parse_article(article_url)
-            htmlcreate(title,divhtml)
+            if divhtml!=[]:
+             htmlcreate(article_url,city,title,divhtml)
 
 
 def parse_article(aurl):
@@ -257,83 +247,93 @@ def parse_article(aurl):
     # 获得网页内容
     summaryPage = driver.page_source
     # 解析HTML内容
-    summaryObj = BeautifulSoup(summaryPage, 'html.parser',from_encoding="utf-8")
+    summaryObj = BeautifulSoup(summaryPage, 'html.parser')
     prety = summaryObj.prettify()
     # print prety
     pointed_div = summaryObj.findAll(name="div",
                                attrs={"class": re.compile("article-box")})
-    if pointed_div==[]:
-     pointed_div = summaryObj.findAll(name="video")
-    #driver.quit()
+    #if pointed_div==[]:
+       # pointed_div = summaryObj.findAll(name="video")
+    driver.quit()
     # 筛选标签为div且属性class为forFlow的源码
     return  pointed_div
 
 
-def htmlcreate(title,divhtml):
-    str+='<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "//www.wapforum.org/DTD/xhtml-mobile10.dtd" > \n'
-    str+='<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN" >\n'
-    str+='<head>\n'
-    str+='<meta name="applicable-device" content="mobile" >\n'
-    str+='<meta http-equiv="Content-Type" content="text/html; charset=utf-8" / >\n'
-    str+='<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" / >\n'
-    str+='<meta name="format-detection" content="telephone=no" / >\n'
-    str+='<meta name="apple-mobile-web-app-capable" content="yes" / >\n'
-    str+='<title> '+title+' </title>\n'
+def htmlcreate(article_url,city,title,divhtml):
+    str1=''
+    str1+='<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "//www.wapforum.org/DTD/xhtml-mobile10.dtd" > \n'
+    str1+='<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN" >\n'
+    str1+='<head>\n'
+    str1+='<meta name="applicable-device" content="mobile" >\n'
+    str1+='<meta http-equiv="Content-Type" content="text/html; charset=utf-8" / >\n'
+    str1+='<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" / >\n'
+    str1+='<meta name="format-detection" content="telephone=no" / >\n'
+    str1+='<meta name="apple-mobile-web-app-capable" content="yes" / >\n'
+    str1+='<title> '+title+' </title>\n'
 
-    str+='<link href="mnewcss.css" rel="stylesheet" type="text/css" / >\n'
-    str+='<link href="toutiao.css" rel="stylesheet" type="text/css" / >\n'
-    str+='</head>\n'
-    str+='<body>\n'
-    str+='<div class="ssi_headB_jxwd_m" ></div>\n'
+    str1+='<link href="../../mnewcss.css" rel="stylesheet" type="text/css" / >\n'
+    str1+='<link href="../../toutiao.css" rel="stylesheet" type="text/css" / >\n'
+    str1+='</head>\n'
+    str1+='<body>\n'
+    str1+='<div class="ssi_headB_jxwd_m" ></div>\n'
 
-    str+='<div class="main" >\n'
+    str1+='<div class="main" >\n'
 
-    str+='<div id="new-style" class="bui-box article-content-container" >\n'
+    str1+='<div id="new-style" class="bui-box article-content-container" >\n'
 
-    str+='<div class="bui-left index-middle" >\n'
+    str1+='<div class="bui-left index-middle" >\n'
 
-    str+=divhtml+'\n'
+    str1+=str(divhtml[0])+'\n'
 
-    str+='</div>\n'
-    str+='</div>\n'
+    str1+='</div>\n'
+    str1+='</div>\n'
 
-    str+='<div class="unfold-field" id="unfold-field" onclick="javascript:showAll()" >\n'
+    str1+='<div class="unfold-field" id="unfold-field" onclick="javascript:showAll()" >\n'
 
-    str+='<div class="unflod-field__mask"> </div>\n'
+    str1+='<div class="unflod-field__mask"> </div>\n'
 
-    str+='<a href="javascript:showAll()" class="unfold-field__text" > 展开全文 </a>\n'
+    str1+='<a href="javascript:showAll()" class="unfold-field__text" > 展开全文 </a>\n'
 
-    str+='</div>\n'
-    str+='</div>\n'
+    str1+='</div>\n'
+    str1+='</div>\n'
 
-    str+='<script>\n'
-    str+='function showAll()\n'
-    str+='{\n'
-    str+='var obj=document.getElementById(\'new-style\');\n'
-    str+='var obja=document.getElementById(\'unfold-field\');\n'
-    str+='obja.style.display="none";\n'
-    str+='obj.style.height="inherit";\n'
-    str+='}\n'
-    str+='</script>\n'
+    str1+='<script>\n'
+    str1+='function showAll()\n'
+    str1+='{\n'
+    str1+='var obj=document.getElementById(\'new-style\');\n'
+    str1+='var obja=document.getElementById(\'unfold-field\');\n'
+    str1+='obja.style.display="none";\n'
+    str1+='obj.style.height="inherit";\n'
+    str1+='}\n'
+    str1+='</script>\n'
+    str1+='<div><a href="'+article_url+'">'+title+'</a></div>\n'
+    str1+='<div class="instop-r">\n'
+    str1+='<div class="icautojz">\n'
+    str1+='<div class="hiddenjz">\n'
 
-    str+='<div class="instop-r">\n'
+    str1+='<ul>\n'
+    str1+='<li>\n'
+    str1+='<a href="https://item.taobao.com/item.htm?id=619450997640&ali_trackid=2:mm_558760164_953650339_109548800308:1597638434_148_2030977526&ak=25016850&bxsign=tbk159763843427910386c83d303cb9b06d649331ade5a4b">\n'
+    str1+='<div class="instop-img" >\n'
+    str1+='<img src="https://gd1.alicdn.com/imgextra/i4/2206835829907/O1CN01t7D6Bw2N3SQTWiXGA_!!2206835829907.jpg" realSrc="https://gd1.alicdn.com/imgextra/i4/2206835829907/O1CN01t7D6Bw2N3SQTWiXGA_!!2206835829907.jpg" />\n'
+    str1+='</div>\n'
+    str1+='<div class="instop-t-new" > 小蛮腰指纹密码智能全自动锁摄像头WiFi王力盼盼星月神万科碧桂园 </div>\n'
+    str1+='<div class="instop-t-time" > 2020-8-08 </div>\n'
+    str1+='</a>\n'
+    str1+='</li>\n'
+    str1+='</ul>\n'
 
-    str+='<div class="icautojz">\n'
+    str1+='</div>\n'
+    str1+='</div>\n'
+    str1+='</div>\n'
 
-    str+='<div class="hiddenjz">\n'
-
-    str+='<ul>\n'
-    str+='<li>\n< a href="https://m.icauto.com.cn/zdm/66/664887.html">\n'
-    str+='<div class="instop-img" >\n'
-    str+='<img src="https://imgs.icauto.com.cn/allimg/randImg/allrand/3675.jpg" realSrc="https://imgs.icauto.com.cn/allimg/randImg/allrand/3675.jpg" / >\n'
-    str+='</div>\n'
-    str+='<div class="instop-t-new" > 减震器坏了有什么症状，汽车减震器坏了的表现 </div>\n'
-    str+='< div class="instop-t-time" > 2019-12-19 < / div >\n'
-    str+='</a>\n'
-    str+='</li>\n'
-    str+='</ul>\n'
-    str+='</div>\n'
-    str+='</div>\n'
-    str+='</div>\n'
-    str+='</body>\n'
-    str+='</html>\n'
+    str1+='</body>\n'
+    str1+='</html>\n'
+    date=time.strftime("%Y%m%d", time.localtime())
+    if os.path.exists("../touxiaoHtml/"+str(date)+"/"+city)==False:
+      os.makedirs("../touxiaoHtml/"+str(date)+"/"+city)
+    title=title.replace("/","")
+    title = title.replace("|", "")
+    fo = open("../touxiaoHtml/"+str(date)+"/"+city+"/"+title+".html", "w+",encoding='utf-8')  # 存入文件中。。。
+    fo.write(str1)
+    fo.close()
